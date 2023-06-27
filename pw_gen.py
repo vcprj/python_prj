c27f95d373f017adb047f5a2212f3413ef68b1ab
vcprj <130110471+vcprj@users.noreply.github.com>
2023-06-27 02:25:45 -0400

    pw generator using secrets

---
+++ pw_gen.py
@@ -0,0 +1,31 @@
+import secrets
+import string
+
+char = string.ascii_letters
+digits = string.digits
+sp_char = string.punctuation
+alpha_num = char + digits + sp_char
+pw_length = 8
+
+while True:
+    user_input = input('generate new password? (y/n): ')
+    if user_input == 'y' or user_input == 'n':
+        break
+    else:
+        print('y or n only')
+
+while user_input == 'y':
+    pw = ''
+    for i in range(pw_length):
+        pw += secrets.choice(alpha_num)
+
+    print('password is: ' + pw)
+
+    while True:
+        user_input = input('generate new password? (y/n): ')
+        if user_input == 'y' or user_input == 'n':
+            break
+        else:
+            print('y or n only')
+
+print('goodbye')
